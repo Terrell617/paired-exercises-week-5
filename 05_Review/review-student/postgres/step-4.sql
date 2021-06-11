2ac64a58e7788616c4cd893598d656d736d5877d
Terrell Mallory <Terrell.Mallory@gmail.com>
2021-06-11 15:01:49 -0400

    week 5 exercises step4

--- 05_Review/review-student/postgres/step-4.sql
+++ 05_Review/review-student/postgres/step-4.sql
@@ -1,11 +1,24 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
-
+SELECT park.name, campground.name, open_from_mm, open_to_mm, daily_fee
+FROM campground
+INNER JOIN park ON campground.campground_id = park.park_id
+ORDER BY park.name, campground.name
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
+SELECT park.name, COUNT(campground.park_id)
+FROM park
+INNER JOIN campground ON park.park_id = campground.campground_id
+GROUP BY park.name
+ORDER BY park.name;
 
 
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
+SELECT park.name, campground.name, site_id, max_occupancy, accessible, max_rv_length, utilities
+FROM site
+INNER JOIN campground On site.site_id = campground.campground_id
+INNER JOIN park ON campground.campground_id = park.park_id
+WHERE campground.name = 'Blackwoods'
 
 /*
   select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
